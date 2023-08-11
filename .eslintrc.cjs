10077c2673d7a0100f3a2d90715ecb73128df9de
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-08-11 00:23:27 -0400

    Linter actualizado

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,18 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
