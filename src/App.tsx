375c93d234bdb5395ba32754ffb21ff7f6aea0df
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-08-11 00:11:05 -0400

    App actualizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,76 @@
+import { useState } from 'react'
+import { TodoTypeCompleted, type TodoID } from './type'
+import { Todos } from './components/Todos'
+import { Footer } from './components/Footer'
+
+const mockTodo = [
+	{
+		id: '1',
+		title: 'Aprender las bases de React',
+		completed: true
+	},
+	{
+		id: '2',
+		title: 'Aprender las bases de JavaScript',
+		completed: true
+	},
+	{
+		id: '3',
+		title: 'Aprender las bases de TypeScript',
+		completed: false
+	},
+	{
+		id: '4',
+		title: 'Crear app con React y TypeScript',
+		completed: false
+	}
+]
 
 const App = (): JSX.Element => {
+	const [todos, setTodos] = useState(mockTodo)
+
+	const handleRemove = ({ id }: TodoID): void => {
+		const newTodos = todos.filter(todo => todo.id !== id)
+		setTodos(newTodos)
+	}
+
+	const handleCompleted = ({ id, completed }: TodoTypeCompleted): void => {
+		const newTodos = todos.map(todo => {
+			if (todo.id === id) {
+				return {
+					...todo,
+					completed
+				}
+			}
+
+			return todo
+		})
+
+		setTodos(newTodos)
+	}
+
+	const removeCompleteds = (): void => {
+		const newTodos = todos.filter(todo => todo.completed !== true)
+		setTodos(newTodos)
+	}
+
 	return (
-		<h1 className="text-center text-2xl">Todo App TypeScript</h1>
+		<>
+			<main className='h-screen flex flex-col items-center p-20'>
+				<h1 className='text-center font-light text-5xl mb-5'>
+					Todo App
+					<span className='text-sky-50 bg-sky-600 font-bold text-2xl pt-3 pl-3 pr-1 rounded ml-1'>TS</span>
+				</h1>
+				<section className='w-2/5 ring-1 ring-slate-300 rounded-lg shadow-2xl shadow-slate-300'>
+					<Todos
+						todos={todos}
+						toggleCompleted={handleCompleted}
+						removeTodo={handleRemove}
+					/>
+					<Footer todos={todos} clearCompleted={removeCompleteds}/>
+				</section>
+			</main>
+		</>
 	)
 }
 
