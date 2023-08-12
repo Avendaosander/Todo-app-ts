0db47d6ecb489553789239ff18f64a7f2ef59f2d
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-08-11 21:58:57 -0400

    Footer Agregado

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,9 @@
+export const Copyright: React.FC = () => (
+	<footer className='absolute bottom-5 sm:left-5 ring-1 ring-slate-500 rounded-[100px] py-2 px-6 text-sm'>
+		<h4 className='font-bold'>
+			Practica de React con TypeScript ⚛️ －
+			<span className='text-sky-600'> @Avendaosander</span>
+		</h4>
+		<h5 className='font-semibold dark:text-slate-300'>Creando un TODO con TypeScript</h5>
+	</footer>
+)
